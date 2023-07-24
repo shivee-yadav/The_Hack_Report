7ff46ba256067a49914eb97214d92cc3d47838e3
shivee-yadav <shiveeyadav089@gmail.com>
2023-07-25 02:36:05 +0530

    Header Done

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Faq />
       <Question />
       <Subscribe />
-      <UI />
+      
       <Footer />
 
     </main>
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
+import  THR4 from "../../assets/THR4.jpg" ;
 
 const Header = () => {
   return (
-    <div>
-    <h1>Header</h1>
-  </div>
+    <section id='header'>
+    <div className="container header">
+        <div className="header-left">
+          <h1>
+            <span>One Stop For All Your Cybersecurity Needs!</span>
+            <span>Your all time cybersecurity partner</span>
+          </h1>
+          <p>Welcome to TheHackReport! We specialize in top-notch penetration testing services, empowering businesses of all sizes
+             and industries to strengthen their security. Unveil vulnerabilities, gain valuable insights, and protect your organization 
+             from cyber threats. Achieve peace of mind with our expert team as your trusted security partner. Contact us today for a secure tomorrow.</p>
+        </div>
+        <div className="header-right">
+          
+
+        </div>
+    </div>
+  </section>
   )
 }
 
