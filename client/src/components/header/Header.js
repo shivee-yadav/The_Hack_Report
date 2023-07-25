f70bf75968088e36feef03541a254e6bcc4b6557
shivee-yadav <shiveeyadav089@gmail.com>
2023-07-25 22:52:30 +0530

    header components added

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./Header.css";
-import  THR4 from "../../assets/THR4.jpg" ;
+import  THR1 from "../../assets/THR1.jpg" ;
+import Button from '../UI/button/Button';
+import '../UI/button/Button.css';
 
 const Header = () => {
   return (
@@ -11,13 +13,17 @@ const Header = () => {
             <span>One Stop For All Your Cybersecurity Needs!</span>
             <span>Your all time cybersecurity partner</span>
           </h1>
-          <p>Welcome to TheHackReport! We specialize in top-notch penetration testing services, empowering businesses of all sizes
+          <p className='u-text-small u-text-light'>
+             Welcome to TheHackReport! We specialize in top-notch penetration testing services, empowering businesses of all sizes
              and industries to strengthen their security. Unveil vulnerabilities, gain valuable insights, and protect your organization 
              from cyber threats. Achieve peace of mind with our expert team as your trusted security partner. Contact us today for a secure tomorrow.</p>
+        <div className="header-cta">
+        <Button text={"Get Started"} btnClass={"btn-dark"} href={"#faq"} />
+        <Button text={"How it works"} btnClass={"btn-orange"} href={"#faq"} />
+        </div>
         </div>
         <div className="header-right">
-          
-
+          <img src={THR1} alt="THR1" />
         </div>
     </div>
   </section>
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -3,6 +3,8 @@ import "./Navbar.css";
 import { SiAnaconda } from "react-icons/si";
 import { AiOutlineBars } from "react-icons/ai";
 import { RiCloseLine } from "react-icons/ri";
+import Button from "../UI/button/Button";
+import "../UI/button/Button.css";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -26,7 +28,8 @@ const Navbar = () => {
           <li><a href="#">Features</a></li>
           <li><a href="#">Download</a></li>
           <li><a href="#">Subscribe</a></li>
-          <li className='nav-btn'><a href="#" className='btn btn-dark'>Get Started</a></li>
+          {/* <li className='nav-btn'><a href="#" className='btn btn-dark'>Get Started</a></li> */}
+          <li className='nav-btn'><Button text={"Learn More"} btnClass={"btn-dark"} href={"#faq"} /></li>
         </ul>
       </menu>
       <div className="menu-icons" onClick={toggleMenu}>
