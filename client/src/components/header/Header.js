8367a2f43e85447e63ea7df7f8510e756b1d709b
shivee-yadav <shiveeyadav089@gmail.com>
2023-07-28 20:17:56 +0530

    new changes in header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Header.css";
-import  THR1 from "../../assets/THR1.jpg" ;
+import  THR2 from "../../assets/THR2.jpg" ;
 import Button from '../UI/button/Button';
 import '../UI/button/Button.css';
 
@@ -16,14 +16,14 @@ const Header = () => {
           <p className='u-text-small u-text-light'>
              Welcome to TheHackReport! We specialize in top-notch penetration testing services, empowering businesses of all sizes
              and industries to strengthen their security. Unveil vulnerabilities, gain valuable insights, and protect your organization 
-             from cyber threats. Achieve peace of mind with our expert team as your trusted security partner. Contact us today for a secure tomorrow.</p>
+             from cyber threats.</p>
         <div className="header-cta">
         <Button text={"Get Started"} btnClass={"btn-dark"} href={"#faq"} />
         <Button text={"How it works"} btnClass={"btn-orange"} href={"#faq"} />
         </div>
         </div>
         <div className="header-right">
-          <img src={THR1} alt="THR1" />
+          <img src={THR2} alt="THR2" />
         </div>
     </div>
   </section>
