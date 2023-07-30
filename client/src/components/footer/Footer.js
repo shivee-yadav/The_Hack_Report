de46998854ca5572d49dbef6767eb62b3cd030e0
shivee-yadav <shiveeyadav089@gmail.com>
2023-07-30 17:37:47 +0530

    Footer Done

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,10 +1,48 @@
-import React from 'react'
-
+import React from 'react';
+import "./Footer.css"
+import  logo from "../../assets/THR-logo.jpg" ;
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <footer id='footer'>
+    <div className='container footer'>
+      <div className="footer-box">
+        <h4>Products</h4>
+        <div className="footer-links">
+          <a href="#">&bull; Web Application Pen Test</a>
+          <a href="#">&bull; Cloud Pen Test</a>
+          <a href="#">&bull; Thick Client Pen Test</a>
+          <a href="#">&bull; API Pen Test</a>
+          <a href="#">&bull; Mobile App Pen Test</a>
+          <a href="#">&bull; IoT Pen Test</a>
+        </div>
+      </div>
+      <div className="footer-box">
+      <h4>News</h4>
+        <div className="footer-links">
+          <a href="#">&bull; Cyber Attacks</a>
+          <a href="#">&bull; Data Breaches</a>
+          <a href="#">&bull; Industry News</a>
+          <a href="#">&bull; Uncategorized</a>
+          <a href="#">&bull; Vulnerabilities</a>
+          <a href="#">&bull; Archives</a>
+        </div>
+      </div>
+      <div className="footer-box">
+      <h4>About</h4>
+        <div className="footer-links">
+          <a href="#">&bull; About Us</a>
+          <a href="#">&bull; Leaders</a>
+          <a href="#">&bull; Customers</a>
+          <a href="#">&bull; Partners</a>
+          <a href="#">&bull; Careers</a>
+          <a href="#subscribe">&bull; Contact Us</a>
+        </div>
+      </div>
+      <div className="footer-box">
+        <img src={logo} alt="logo" />
+      </div>
     </div>
+    </footer>
   )
 }
 
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -25,9 +25,9 @@ const Navbar = () => {
       <menu>
         <ul className="nav-links" id={showMenu ? "nav-links-mobile" : "nav-links-mobile-hide"}>
           <li><a href="#">Home</a></li>
-          <li><a href="#">Features</a></li>
-          <li><a href="#">Download</a></li>
-          <li><a href="#">Subscribe</a></li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#download">Download</a></li>
+          <li><a href="#subscribe">Subscribe</a></li>
           {/* <li className='nav-btn'><a href="#" className='btn btn-dark'>Get Started</a></li> */}
           <li className='nav-btn'><Button text={"Learn More"} btnClass={"btn-dark"} href={"#faq"} /></li>
         </ul>
