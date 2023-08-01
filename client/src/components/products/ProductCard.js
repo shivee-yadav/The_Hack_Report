eae368abc9f85b0218e660991cdd996fee12e873
shivee-yadav <shiveeyadav089@gmail.com>
2023-08-01 23:08:09 +0530

    new change

--- client/src/components/products/ProductCard.js
+++ client/src/components/products/ProductCard.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import "./ProductCard.css";
+
+
 
 const ProductCard = () => {
   return (
     <div>
       <h1>ProductCard</h1>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
