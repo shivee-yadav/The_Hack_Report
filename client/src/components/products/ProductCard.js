78cc653cb03d7501bc0902d07be1c4d9a7754a2f
shivee-yadav <shiveeyadav089@gmail.com>
2023-08-05 00:56:28 +0530

    new changes

--- client/src/components/products/ProductCard.js
+++ client/src/components/products/ProductCard.js
@@ -8,11 +8,12 @@ import "../UI/button/Button.css";
 const ProductCard = ({img,heading,text}) => {
   return (
    
-    <div class="card" >
+    <div class="card" data-aos="fade-right" >
       <div class="card-img-top">{img}</div>
       <div class="card-body">
       <h5 class="card-title">{heading}</h5>
       <p class="card-text">{text}</p>
+      <a href="#" class='card-link'></a>
     </div>
     <div className="button">
     <Button text={"Learn More"} btnClass={"btn-orange"} href={"#faq"} />
--- client/src/components/products/data.js
+++ client/src/components/products/data.js
@@ -3,25 +3,25 @@ export const productList = [
     {
       id: 1,
       img: <img src={iconBugBounty} alt="bug-bounty" />,
-      heading: "40% REDUCED QA COSTS" ,
-      text: "We provide quality verification of software products and services with the help of our manual QA testing approach to significantly reduce QA costs"
+      heading: "Bug Bounty" ,
+      text: "Continuously find hidden flaws by collaborating with security researchers matched to your precise needs."
     },
     {
       id: 2,
       img: <img src={iconBugBounty} alt="bug-bounty" />,
-      heading: "QUALITY ENGINEERING" ,
-      text: "Our quality engineering services makes sure testing shifts left with a purpose of maintaining maximum test coverage and software quality"
+      heading: "Pen Test as a Service" ,
+      text: "Configure pen tests rapidly launch in days and see high impact results from elite teams in real time."
     },
     {
       id: 3,
       img: <img src={iconBugBounty} alt="bug-bounty" />,
-      heading: "GLOBAL DELIVERY MODEL" ,
-      text: "We accommodate a global delivery model via several onshore test centers spread across different countries"
+      heading: "Vulnerability Disclosure" ,
+      text: "Prove you do everything possible to protect takeholders by accepting vulnerability reports from the public."
     },
     {
       id: 4,
       img: <img src={iconBugBounty} alt="bug-bounty" />,
-      heading: "GLOBAL DELIVERY MODEL" ,
-      text: "We accommodate a global delivery model via several onshore test centers spread across different countries"
+      heading: "Attack Surface Management" ,
+      text: "Discover and analyse asset risks, relying on security researcher ingenuity to find them before attackers."
     }
   ]
