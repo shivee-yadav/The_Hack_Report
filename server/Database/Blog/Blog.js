8dd192c3ffcf23de5a39b3aab529f2ec24c44fd9
Shivee Yadav <shiveeyadav089@gmail.com>
2023-09-09 23:24:58 +0530

    blog

---
+++ server/Database/Blog/Blog.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const blogSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+	description: { type: String,  required: true },
+	image: { type: String },
+    user: {type: String, required: true}
+},
+{ timestamps : true }//automatically created and updated
+);
+
+
+
+const validate = (blog) => {
+	const schema = Joi.object({
+		title: Joi.string().required(),
+		description: Joi.string().required(),
+		image:Joi.string()
+	});
+	return schema.validate(blog);
+};
+
+const BlogModel = mongoose.model("Blog", blogSchema);
+
+module.exports = { BlogModel, validate };
