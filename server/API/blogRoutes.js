ef1dc95b04a5f097a86be801df57cd0f40cde9b8
Shivee Yadav <shiveeyadav089@gmail.com>
2023-09-10 02:12:33 +0530

    Routes for blogs

--- server/API/blogRoutes.js
+++ server/API/blogRoutes.js
@@ -17,4 +17,79 @@ Router.get("/", async(req,res,next) => {
     return res.status(200).json({blogs});
 })
 
+Router.post("/add",async(req,res,next) => {
+   const { title, description, image, user} = req.body;
+   const blog = new BlogModel({
+    title,
+    description,
+    image,
+    user,
+   });
+
+    try {
+        await blog.save(); 
+    } catch (error) {
+        return console.log(error)
+        
+    }
+    
+    return res.status(200).json({blog});
+})
+
+Router.put("/update/:id",async(req,res,next) => {
+
+   const { title, description, image} = req.body;
+   const blogId = req.params.id;
+   let blog;
+   
+
+    try {
+        blog = await BlogModel.findByIdAndUpdate(blogId, {
+            title,
+            description,
+            image,
+           });
+
+    } catch (error) {
+        return console.log(error)
+        
+    }
+    if(!blog){
+        return res.status(500).json({message:"Unable to update the blog"})
+    }
+    
+    return res.status(200).json({blog});
+})
+
+Router.get("/:id", async(req,res,next) => {
+    const id=req.params.id;
+    let blog;
+    try {
+        blog = await BlogModel.findById(id);
+    } catch (error) {
+        return console.log(error)
+    }
+    if(!blog){
+        return res.status(404).json({message:"No blog found"})
+    }
+    
+    return res.status(200).json({blog});
+})
+
+
+Router.delete("/:id", async(req,res,next) => {
+    const id=req.params.id;
+    let blog;
+    try {
+        blog = await BlogModel.findByIdAndDelete(id);
+    } catch (error) {
+        return console.log(error)
+    }
+    if(!blog){
+        return res.status(500).json({message:"Unable to delete"})
+    }
+    
+    return res.status(200).json({message:"Successfully deleted"});
+})
+
 export default Router;
